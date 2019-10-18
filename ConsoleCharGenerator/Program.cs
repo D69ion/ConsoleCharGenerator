4e8cdedf76b782e6eb1435fc62a136fe8a2836bc
dmitriy <dmitriy.shtele@gmail.com>
2019-10-18 14:43:03 +0400

    probable, generator is done

--- ConsoleCharGenerator/Program.cs
+++ ConsoleCharGenerator/Program.cs
@@ -9,37 +9,33 @@ namespace ConsoleCharGenerator
 {
     class Program
     {
-        static Random random = new Random();
+        private static Random random = new Random();
         static void Main(string[] args)
         {
             string path = "";
-            char[] alphabet;
-            double[] probabilities;
+            //Tuple<char, double>[] tuple = new Tuple<char, double>[10];
+            List<Tuple<char, double>> tuples = new List<Tuple<char, double>>();
             using (StreamReader reader = new StreamReader(path))
             {
-                alphabet = reader.ReadLine().Remove(' ').ToCharArray();
-                probabilities = ToDoubleArray(reader.ReadLine().Split(' '));
+                string[] temp;
+                while (!reader.EndOfStream)
+                {
+                    temp = reader.ReadLine().Split(' ');
+                    tuples.Add(new Tuple<char, double>(Convert.ToChar(temp[0]), Convert.ToDouble(temp[1])));
+                }
             }
-
-
-
-            //SortedList<char, double> valuePairs = new SortedList<char, double>();//попытка пофиксить неупорядоченность, но вероятности у символов могут быть одинаковыми
-            //for (int i = 0; i < alphabet.Length; i++)
-            //{
-            //    valuePairs.Add(alphabet[i], probabilities[i]);
-            //}
-
-
+            tuples.TrimExcess();
+            QSort(ref tuples, 0, tuples.Count - 1);
 
             StringBuilder result = new StringBuilder();
-            for (int i = 0; i < 100; i++)
+            for (int i = 0; i < random.Next(60, 200); i++)
             {
                 double q = random.NextDouble();
-                for (int j = 1; j < alphabet.Length; j++)
+                for (int j = 1; j < tuples.Count; j++)
                 {
-                    if (q > probabilities[j - 1] && q < probabilities[j])//не факт что оно упорядочено
+                    if (q > tuples[j - 1].Item2 && q < tuples[j].Item2)
                     {
-                        result.Append(alphabet[j]);
+                        result.Append(tuples[j].Item1);
                     }
                 }
             }
@@ -48,7 +44,45 @@ namespace ConsoleCharGenerator
             Console.ReadLine();
         }
 
-        static double[] ToDoubleArray(string[] src)
+        private static void QSort(ref List<Tuple<char, double>> tuples, int i, int j)
+        {
+            if (i < j)
+            {
+                int q = Partition(ref tuples, i, j);
+                QSort(ref tuples, i, q);
+                QSort(ref tuples, q + 1, j);
+            }
+        }
+
+        private static int Partition(ref List<Tuple<char, double>> tuples, int p, int r)
+        {
+            double x = tuples[p].Item2;
+            int i = p - 1;
+            int j = r + 1;
+            while (true)
+            {
+                do
+                {
+                    j--;
+                }
+                while (tuples[j].Item2 > x);
+                do
+                {
+                    i++;
+                }
+                while (tuples[i].Item2 < x);
+                if (i < j)
+                {
+                    (tuples[i], tuples[j]) = (tuples[j], tuples[i]);
+                }
+                else
+                {
+                    return j;
+                }
+            }
+        }
+
+        private static double[] ToDoubleArray(string[] src)
         {
             double[] res = new double[src.Length];
             for (int i = 0; i < src.Length; i++)
